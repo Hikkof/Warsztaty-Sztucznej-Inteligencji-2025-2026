4640ff4cddf28978d34872c9b6d3796a195a316a
Hikkof <108770694+Hikkof@users.noreply.github.com>
2026-05-20 21:58:34 +0200

    Refactor activity data filtering and processing

--- wstepne czyszczenie/activity.py
+++ wstepne czyszczenie/activity.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 from chembl_webresource_client.new_client import new_client
 
@@ -8,7 +9,7 @@ activity = new_client.activity
 records_num = len(activity)
 field_names = list(activity.all()[0])
 
-# pola które z jakiegoś powodu nie pozwalają na filtrowanie
+# pola które z jakiegoś powodu nie pozwalają na filtrowanie używając __isnull
 fields_n = ['bao_format', 'bao_label', 'data_validity_description', 'src_id', 'toid']
 for field in fields_n:
     field_names.remove(field)
@@ -21,19 +22,21 @@ plt.xlabel('Percentage of nulls')
 plt.ylabel('Field names')
 plt.show()
 
-activity = activity.filter(standard_relation__iexact="=").filter(canonical_smiles__isnull=False).filter(value__isnull=False)
-activity = activity.filter(standard_type__iexact="IC50")
 
-df_activity = pd.DataFrame(activity[:10000])
+activity = activity.filter(canonical_smiles__isnull=False,
+                           standard_relation='=',
+                           standard_type__in=['IC50', 'pIC50'],
+                           standard_units__in=['nM', 'uM'],
+                           standard_value__isnull=False,
+                           target_chembl_id='CHEMBL230') \
+    .only(['activity_id', 'assay_chembl_id', 'assay_type', 'bao_endpoint', 'bao_format', 'bao_label',
+           'canonical_smiles', 'molecule_chembl_id', 'qudt_units', 'standard_type', 'standard_units', 'standard_value',
+           'target_chembl_id', 'target_organism', 'target_pref_name', 'target_tax_id', 'type', 'units', 'uo_units',
+           'value'])
 
-df_activity.drop([field_names[field_num] for field_num in range(len(field_names)) if null_counts[field_num] >= 0.50],
-                 axis=1, inplace=True)
+df_activity = pd.DataFrame(activity)
 
-to_drop = ['activity_comment', 'assay_description', 'data_validity_description', 'target_pref_name', 'toid']
-df_activity.drop(to_drop, axis=1, inplace=True)
-
-ids = ['assay_chembl_id', 'bao_endpoint', 'bao_format', 'document_chembl_id', 'molecule_chembl_id',
-       'parent_molecule_chembl_id', 'target_chembl_id', 'uo_units']
+ids = ['assay_chembl_id', 'bao_endpoint', 'bao_format', 'molecule_chembl_id', 'target_chembl_id', 'uo_units']
 for i in ids:
     df_activity[i] = df_activity[i].str.extract('(\d+)', expand=False).astype('Int32')
 
@@ -42,32 +45,24 @@ df_activity.to_csv('activity.csv', index=False)
 print(df_activity.info())
 
 '''
- 0   activity_id                10000 non-null  int64 
- 1   assay_chembl_id            10000 non-null  Int32 
- 2   assay_type                 10000 non-null  object
- 3   bao_endpoint               10000 non-null  Int32 
- 4   bao_format                 10000 non-null  Int32 
- 5   bao_label                  10000 non-null  object
- 6   canonical_smiles           10000 non-null  object
- 7   document_chembl_id         10000 non-null  Int32 
- 8   document_year              10000 non-null  int64 
- 9   molecule_chembl_id         10000 non-null  Int32 
- 10  parent_molecule_chembl_id  10000 non-null  Int32 
- 11  potential_duplicate        10000 non-null  int64 
- 12  qudt_units                 7395 non-null   object
- 13  record_id                  10000 non-null  int64 
- 14  relation                   10000 non-null  object
- 15  src_id                     10000 non-null  int64 
- 16  standard_flag              10000 non-null  int64 
- 17  standard_relation          10000 non-null  object
- 18  standard_type              10000 non-null  object
- 19  standard_units             8376 non-null   object
- 20  standard_value             10000 non-null  object
- 21  target_chembl_id           10000 non-null  Int32 
- 22  target_organism            8783 non-null   object
- 23  target_tax_id              8783 non-null   object
- 24  type                       10000 non-null  object
- 25  units                      7911 non-null   object
- 26  uo_units                   7809 non-null   Int32 
- 27  value                      10000 non-null  object
+ 0   activity_id         6283 non-null   int64 
+ 1   assay_chembl_id     6283 non-null   Int32 
+ 2   assay_type          6283 non-null   object
+ 3   bao_endpoint        6283 non-null   Int32 
+ 4   bao_format          6283 non-null   Int32 
+ 5   bao_label           6283 non-null   object
+ 6   canonical_smiles    6283 non-null   object
+ 7   molecule_chembl_id  6283 non-null   Int32 
+ 8   qudt_units          6283 non-null   object
+ 9   standard_type       6283 non-null   object
+ 10  standard_units      6283 non-null   object
+ 11  standard_value      6283 non-null   object
+ 12  target_chembl_id    6283 non-null   Int32 
+ 13  target_organism     6283 non-null   object
+ 14  target_pref_name    6283 non-null   object
+ 15  target_tax_id       6283 non-null   object
+ 16  type                6283 non-null   object
+ 17  units               5687 non-null   object
+ 18  uo_units            6283 non-null   Int32 
+ 19  value               6283 non-null   object
 '''
