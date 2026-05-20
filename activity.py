a047c91c6502c78c3640848c1365bb04be1069a3
Hikkof <108770694+Hikkof@users.noreply.github.com>
2026-05-20 22:02:04 +0200

    Refactor activity data processing and IC50 conversion
    Refactor data processing in activity.py to remove unnecessary columns and convert IC50 values to pIC50.

--- activity.py
+++ activity.py
@@ -1,33 +1,42 @@
+# jeśli ostatecznie będziemy przyjmować tylko 'SMILES' i z niego wydobywać dodatkowe informacje to ten plik jest zbędny
+
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
 
 
+### wiem że mogę użyć df_activity[[cecy_które_chcę]], ale jest tutaj wytłumaczenie dlaczego biorę konkretne cechy
 df = pd.read_csv('initial cleaning/activity.csv', index_col=False)
 
 # wyrzucam kolumny które (prawie) na pewno nie będą przydatne
-df['potential_duplicate'] = df['potential_duplicate'].astype('bool')
-# nie sądzę że te informacje są przydatne
-df.drop('document_chembl_id', axis=1, inplace=True)
-df.drop('document_year', axis=1, inplace=True)
-# jedyna wartość to 1
-df.drop('src_id', axis=1, inplace=True)
-# 'standard_flag' nie wygląda jak ważna informacja
-df.drop('standard_flag', axis=1, inplace=True)
-# używam 'relation', 'standard_relatio' nie jest potrzebne
-df.drop('standard_relation', axis=1, inplace=True)
-# 'record_id' nie wygląda jak ważna informacja
-df.drop('record_id', axis=1, inplace=True)
-# używam 'unints', 'qudt_units' i 'standard_units' nie jest potrzebne
-df.drop('qudt_units', axis=1, inplace=True)
+# zamienianie IC50 na pIC50
+df = df[df['standard_value'] > 0]
+df.loc[(df['standard_type'] == 'IC50') & (df['standard_units'] == 'uM'), 'standard_value'] = -np.log10(df['standard_value'] / 1e6)
+df.loc[(df['standard_type'] == 'IC50') & (df['standard_units'] == 'nM'), 'standard_value'] = -np.log10(df['standard_value'] / 1e9)
+
+#df['pIC50 '] = np.log(df['IC50'] * 0.001) * (-1)
+#df.loc[df['standard_type'] == 'IC50', 'standard_value'] = 10**(-1*df['standard_value'])
+# te informacje już nie są potrzebne
 df.drop('standard_units', axis=1, inplace=True)
-# 'target_organism' i 'target_tax_id' są wybrakowane, 'target_chembl_id' jest wystarczające
-df.drop('target_organism', axis=1, inplace=True)
-df.drop('target_tax_id', axis=1, inplace=True)
-# używam 'standard_type', 'type' nie jest potrzebne
-df.drop('type', axis=1, inplace=True)
-# 'uo_units' nie wygląda jak ważna informacja
+df.drop('units', axis=1, inplace=True)
 df.drop('uo_units', axis=1, inplace=True)
+df.drop('qudt_units', axis=1, inplace=True)
+
+df.drop('target_chembl_id', axis=1, inplace=True)
+df.drop('target_organism', axis=1, inplace=True)
+df.drop('target_pref_name', axis=1, inplace=True)
+df.drop('target_tax_id', axis=1, inplace=True)
+
+df.drop('standard_type', axis=1, inplace=True)
+df.drop('type', axis=1, inplace=True)
+
 # używam 'standard_value', 'value' nie jest potrzebne
 df.drop('value', axis=1, inplace=True)
+
+# nadal nie wygląda prawidłow, ale nie wiem jakie wartości 'standard_value' powinno przyjmować
+plt.boxplot(df['standard_value'])
+plt.show()
 
 print(df.info())
 
@@ -43,10 +52,6 @@ df.to_csv('activity.csv', index=False)
  6   canonical_smiles           10000 non-null  object 
  7   molecule_chembl_id         10000 non-null  int64  
  8   parent_molecule_chembl_id  10000 non-null  int64  
- 9   potential_duplicate        10000 non-null  bool   
- 10  relation                   10000 non-null  object 
- 11  standard_type              10000 non-null  object 
- 12  standard_value             10000 non-null  float64
- 13  target_chembl_id           10000 non-null  int64  
- 14  units                      7911 non-null   object 
+ 9   standard_value             10000 non-null  float64
+ 10  target_chembl_id           10000 non-null  int64  
  '''
