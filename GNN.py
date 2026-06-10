873446f3b5322dbb250ac7aaa0a6341cbb449f1c
Hikkof <108770694+Hikkof@users.noreply.github.com>
2026-06-10 21:39:08 +0200

    Refactor GNN model and update data processing

--- GNN.py
+++ GNN.py
@@ -2,7 +2,6 @@ from abc import ABC
 
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 import torch
 import torch.nn as nn
 from torch_geometric.nn import GCNConv, global_mean_pool
@@ -18,9 +17,10 @@ class GNN(nn.Module):
     def __init__(self, in_channels):
         super(GNN, self).__init__()
 
-        self.conv1 = GCNConv(in_channels, 64)
-        self.conv2 = GCNConv(64, 64)
-
+        self.conv1 = GCNConv(in_channels, 256)
+        self.conv2 = GCNConv(256, 128)
+        self.conv3 = GCNConv(128, 64)
+        self.conv4 = GCNConv(64, 64)
         self.lin1 = nn.Linear(64, 32)
         self.lin2 = nn.Linear(32, 1)
 
@@ -28,8 +28,8 @@ class GNN(nn.Module):
         x, edge_index, batch = data.x, data.edge_index, data.batch
         x = self.conv1(x, edge_index).relu()
         x = self.conv2(x, edge_index).relu()
-        x = self.conv2(x, edge_index).relu()
-
+        x = self.conv3(x, edge_index).relu()
+        x = self.conv4(x, edge_index).relu()
         x = global_mean_pool(x, batch)
         x = self.lin1(x).relu()
         x = self.lin2(x)
@@ -40,17 +40,29 @@ class GNN(nn.Module):
 def mol_to_graph(mol, target):
     atom_features = []
     for atom in mol.GetAtoms():
-        atom_features.append([
-            atom.GetAtomicNum(),
-            atom.GetTotalDegree(),
-            atom.GetFormalCharge(),
-            atom.GetTotalNumHs(),
-            atom.GetHybridization().real,
-            atom.GetIsAromatic()
-        ])
+        atom_features.append(
+            funkcje.one_hot(atom.GetSymbol(), funkcje.ATOMIC_VOCAB) +
+            funkcje.one_hot(atom.GetChiralTag(), funkcje.DEGREE_VOCAB) +
+            funkcje.one_hot(atom.GetTotalDegree(), funkcje.DEGREE_VOCAB) +
+            funkcje.one_hot(atom.GetHybridization(), funkcje.HYBRIDIZATION_VOCAB) + [
+                atom.GetFormalCharge(),
+                atom.GetTotalNumHs(),
+                atom.GetMass(),
+                atom.GetDegree(),
+                atom.GetTotalNumHs(),
+                atom.GetValence(which=Chem.ValenceType.EXPLICIT),
+                atom.GetValence(which=Chem.ValenceType.IMPLICIT),
+                atom.GetNumRadicalElectrons(),
+                atom.GetIsotope(),
+                atom.HasProp('_CIPCode'),
+                atom.GetIsAromatic(),
+                atom.IsInRing(),
+            ])
     x = torch.tensor(atom_features, dtype=torch.float)
 
     edge_index = []
+    edge_attr = []
+
     for bond in mol.GetBonds():
         i = bond.GetBeginAtomIdx()
         j = bond.GetEndAtomIdx()
@@ -58,11 +70,23 @@ def mol_to_graph(mol, target):
         edge_index.append([i, j])
         edge_index.append([j, i])
 
+        bond_features = \
+            funkcje.one_hot(bond.GetBondType(), funkcje.BOND_TYPE_VOCAB) + \
+            funkcje.one_hot(bond.GetStereo(), funkcje.STEREO_VOCAB) + [
+                bond.GetIsConjugated(),
+                bond.IsInRing(),
+                bond.GetIsAromatic()
+            ]
+
+        edge_attr.append(bond_features)
+        edge_attr.append(bond_features)
+
     edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
+    edge_attr = torch.tensor(edge_attr, dtype=torch.float)
 
     y = torch.tensor([target], dtype=torch.float)
 
-    return Data(x=x, edge_index=edge_index, y=y)
+    return Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
 
 
 class CustomDataset(InMemoryDataset, ABC):
@@ -70,22 +94,25 @@ class CustomDataset(InMemoryDataset, ABC):
         super().__init__()
         data_list = []
 
-        for smiles, target in zip(data_frame["canonical_smiles"], data_frame["standard_value"]):
-            graph = mol_to_graph(smiles, target)
+        for mol, target in zip(data_frame["mol"], data_frame["standard_value"]):
+            graph = mol_to_graph(mol, target)
             data_list.append(graph)
 
         self.data, self.slices = self.collate(data_list)
 
 
-df_activity = pd.read_csv('initial cleaning/activity.csv.csv', index_col=False)
+df_activity = pd.read_csv('activity.csv', index_col=False)
 df_activity = df_activity[['canonical_smiles', 'standard_value']]
 
-df_activity['canonical_smiles'] = df_activity['canonical_smiles'].apply(Chem.MolFromSmiles)
-df_activity['canonical_smiles'].dropna(inplace=True)
+df_activity["mol"] = df_activity["canonical_smiles"].apply(Chem.MolFromSmiles)
+# rdkit wpisuje 'None' w niemożliwych cząsteczkach
+df_activity = df_activity.dropna(subset=["mol"]).reset_index(drop=True)
 
-epochs = 250
-loss_functions = {'MSELoss': nn.MSELoss(), 'R2Score': R2Score()}
-split_types = {'Random': funkcje.random_split(df_activity), 'Scaffold': funkcje.scaffold_split_df(df_activity)}
+epochs = 100
+loss_function = nn.MSELoss()
+r2 = R2Score()
+split_types = {'Random': funkcje.random_split(df_activity),
+               'Scaffold': funkcje.scaffold_split_df(df_activity, mol_col='mol')}
 
 for split in split_types:
     print(f"Rodzaj podziału: {split}")
@@ -94,49 +121,60 @@ for split in split_types:
     train_dataset = CustomDataset(train)
     train_loader = DataLoader(train_dataset, batch_size=funkcje.BATCH_SIZE, shuffle=True)
 
+    valid_dataset = CustomDataset(valid)
+    valid_loader = DataLoader(valid_dataset, batch_size=funkcje.BATCH_SIZE, shuffle=True)
+
     test_dataset = CustomDataset(test)
     test_loader = DataLoader(test_dataset, batch_size=funkcje.BATCH_SIZE, shuffle=True)
 
-    for loss_function in loss_functions:
-        print(f"Funkcja straty: {loss_function}")
+    model = GNN(in_channels=test_dataset[0].x.shape[1])
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 
-        model = GNN(in_channels=test_dataset[0].x.shape[1])
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+    train_losses_MSE = []
+    valid_losses_MSE = []
+    valid_losses_R2 = []
+    test_mse = 0
 
-        train_losses = []
-        test_losses = []
+    for epoch in range(epochs):
+        model.train()
+        train_mse = []
 
-        for epoch in range(epochs):
-            model.train()
-            train_losses.append([])
+        for batch in train_loader:
+            optimizer.zero_grad()
+            outputs = model(batch)
 
-            for batch in train_loader:
-                optimizer.zero_grad()
+            loss = loss_function(outputs, batch.y)
+            train_mse.append(loss.item())
+
+            loss.backward()
+            optimizer.step()
+
+        model.eval()
+        r2.reset()
+        valid_mse = []
+
+        with torch.no_grad():
+            for batch in valid_loader:
                 outputs = model(batch)
+                valid_mse.append(loss_function(outputs, batch.y).item())
+                r2.update(outputs, batch.y)
 
-                loss = loss_functions[loss_function](outputs, batch.y)
-                train_losses[epoch].append(loss.item())
+        train_losses_MSE.append(sum(train_mse) / len(train_mse))
+        valid_losses_MSE.append(sum(valid_mse) / len(valid_mse))
+        valid_losses_R2.append(r2.compute().item())
 
-                loss.backward()
-                optimizer.step()
+    with torch.no_grad():
+        for batch in test_loader:
+            outputs = model(batch)
+            test_mse = (loss_function(outputs, batch.y).item())
 
-            model.eval()
-            test_losses.append([])
+    plt.plot(train_losses_MSE)
+    plt.plot(valid_losses_MSE)
+    plt.show()
 
-            with torch.no_grad():
-                for batch in test_loader:
-                    outputs = model(batch)
-                    loss = loss_functions[loss_function](outputs, batch.y)
-                    test_losses[epoch].append(loss.item())
+    print(f"Najniższy MSE treningowy: {min(train_losses_MSE)}")
+    print(f"Najniższy MSE validacyjny: {min(valid_losses_MSE)}")
+    print(f"MSE testowy: {test_mse}")
 
-        train_average = [sum(e) / len(e) for e in train_losses]
-        test_average = [sum(e) / len(e) for e in test_losses]
-        '''
-        plt.plot(train_average)
-        plt.plot(test_average)
-        plt.show()
-        '''
-        print(f"Najniższy wynik treningowy: {min(train_average)}")
-        print(f"Najniższy wynik testowy: {min(test_average)}")
-
-print('?')
+    print(f"R2: {max(valid_losses_R2)}")
+    print(valid_losses_R2)
